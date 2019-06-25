085d8521a5f51c1c1618aa154b7fc0de1165ac4b
Tomoki <tech54tu@gmail.com>
2019-06-25 19:44:14 +0900

    edit quizzes column

--- db/migrate/20190625103904_create_quizzes.rb
+++ db/migrate/20190625103904_create_quizzes.rb
@@ -1,7 +1,9 @@
 class CreateQuizzes < ActiveRecord::Migration[5.2]
   def change
     create_table :quizzes do |t|
-
+      t.string :title,       null: false, unique: true
+      t.text   :description
+      t.string :image
       t.timestamps
     end
   end
