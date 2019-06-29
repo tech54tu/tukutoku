5bebce8e4a5558452d53eec106bfd4f08daf741d
Tomoki <tech54tu@gmail.com>
2019-06-29 21:20:46 +0900

    create action

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,4 +2,17 @@ class QuizzesController < ApplicationController
   def index
     @quizzes = Quiz.all
   end
+
+  def new
+  end
+
+  def create
+    Quiz.create(quiz_params)
+    redirect_to action: :index
+  end
+
+  private
+  def quiz_params
+    params.permit(:title, :description, :image)
+  end
 end
