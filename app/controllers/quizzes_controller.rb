7e6677a0400ae36481fa7ce4484a345b2f5dc34b
Tomoki <tech54tu@gmail.com>
2019-06-30 14:37:51 +0900

    Update quizzes_controller.rb

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,15 +4,21 @@ class QuizzesController < ApplicationController
   end
 
   def new
+    @quiz = Quiz.new
+    # @quiz.questions.build
   end
 
   def create
     Quiz.create(quiz_params)
+    # @quiz = Quiz.create(title: quiz_params[:title], description: quiz_params[:description], image: quiz_params[:image], )
+    # @quiz = Quiz.new(quiz_params)
+    # @quiz = Quiz.save
     redirect_to action: :index
   end
 
   private
   def quiz_params
-    params.permit(:title, :description, :image)
+    params.require(:quiz).permit(:title, :description, :image)    
+    # params.require(:quiz).permit(:title, :description, :image, question_attributes: [:id, :question, :answer])
   end
 end
